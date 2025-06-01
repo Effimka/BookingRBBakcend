52cf997bdb4b4d0dd8659e51944ff67b333a1e9c
tix01 <tihonvolk0@gmail.com>
2025-06-01 17:19:03 +0300

    test1

--- app/api/user.py
+++ app/api/user.py
@@ -3,6 +3,7 @@ from schemas.userShemas import UserCreate, UserOut
 
 router = APIRouter()
 
-@router.post("/api/auth/register", response_model=UserOut)
+@router.post("/register", response_model=UserOut)
 async def create_user_api(user: UserCreate):
     print(f"User comming email = {user.email} pass = {user.password}")
+    return {"id": 1, "name": "tix", "email" : user.email}
--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from api import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # Разрешить только указанные источники
+    allow_credentials=True,
+    allow_methods=["*"],            # Разрешить любые методы: GET, POST и т.д.
+    allow_headers=["*"],            # Разрешить любые заголовки
+)
 
 @app.on_event("startup")
 async def startup():
