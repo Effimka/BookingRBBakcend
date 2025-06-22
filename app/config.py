4e5890005c72eec8c7e0c72ad7dcb087eb29cf4f
Effimka <groshevskiy2002@mail.ru>
2025-06-22 20:33:41 +0400

    add jwt implement

--- app/config.py
+++ app/config.py
@@ -5,5 +5,16 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+#JWT vars
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM_JWT = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not set in environment")
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not set in environment")
+
