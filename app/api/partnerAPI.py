3c25c25c46d461ef3fae7457cb32dd9528736dc1
tix01 <tihonvolk0@gmail.com>
2025-07-04 15:36:48 +0300

    iter4

--- app/api/partnerAPI.py
+++ app/api/partnerAPI.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.dependencyDB import get_db
 from schemas.partnerShemas import PartnerCreate, AgentPartnerOutWhitToken
-from crud.userCRUD import create_user, get_user_by_email
+from crud.partnerCRUD import create_partner_with_agent
 from auth.jwtHandler import create_access_token, create_refresh_token
 from Common.CommonToken import setResponseRefreshToken
 
@@ -10,17 +10,8 @@ router = APIRouter()
 
 @router.post("/register-parthner", response_model=AgentPartnerOutWhitToken)
 async def create_partner_api(user: PartnerCreate, response: Response, db: AsyncSession  = Depends(get_db)):
-    userId, userOutData = await create_user(user, db)
+    userId, userOutData = await create_partner_with_agent(user, db)
     accesToken = create_access_token(data={"sub":str(userId)})
     refreshToken = create_refresh_token(data={"sub":str(userId)})
     setResponseRefreshToken(response, refreshToken)
     return AgentPartnerOutWhitToken(userOutData, accesToken)
-
-
-@router.post("/login-parthner", response_model=UserOutWithToken)
-async def login_user_api(user: UserCreate, response: Response, db: AsyncSession  = Depends(get_db)):
-    userId, userOutData = await get_user_by_email(user, db)
-    accesToken = create_access_token(data={"sub":str(userId)})
-    refreshToken = create_refresh_token(data={"sub":str(userId)})
-    setResponseRefreshToken(response, refreshToken)
-    return UserOutWithToken(userOutData, accesToken)
