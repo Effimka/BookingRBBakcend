1cbee8a9042c156ac3ffaf5fef2c85a850a76c40
tix01 <tihonvolk0@gmail.com>
2025-07-04 15:36:48 +0300

    iter4

--- app/crud/partnerCRUD.py
+++ app/crud/partnerCRUD.py
@@ -1,10 +1,44 @@
-from schemas.partnerShemas import PartnerCreate, AgentPartnerOut
-from models.partnerModel import Partner, AgentPartner
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
-from sqlalchemy import select
+from models import Partner, AgentPartner
+from schemas import PartnerCreate, AgentPartnerOut
 
-async def create_partner(partner: PartnerCreate, db: AsyncSession):
+async def create_partner_with_agent(partner_data: PartnerCreate, db: AsyncSession):
     try:
-        db_partner = 
+        db_partner = Partner(
+            phone_number=partner_data.phone_number,
+            lawcode=partner_data.lawcode,
+            data_registration=partner_data.data_registration
+        )
+        db.add(db_partner)
+        await db.flush() 
+
+        agent = partner_data.agentPartnerCreateBaseData
+        db_agent = AgentPartner(
+            partner_id=db_partner.id,
+            role_code=agent.role_code,
+            email=agent.email,
+            password=agent.password, 
+            lastname=agent.lastname,
+            firstname=agent.firstname
+        )
+        db.add(db_agent)
+
+        await db.commit()
+        await db.refresh(db_agent)
+
+        return AgentPartnerOut.model_validate(db_agent)
+
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists or other constraint failed"
+        )
+    except Exception:
+        await db.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail="Unknown error during partner creation"
+        )
